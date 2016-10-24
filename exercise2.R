437974eccb9a31de1d51702aa1aa0550087898c2
Maggi <maggi@Maggis-MacBook-Pro.local>
2016-10-24 12:40:08 -0600

    function that works. I think.

--- exercise2.R
+++ exercise2.R
@@ -12,59 +12,28 @@ for(i in 20:10){
   print(i)
 }
 
-#3. Write a function that calculates whether a number is a prime number (hint: what does 2 %% 3 give you?)
-num = as.integer(3)
-
-b = 0
-if(num > 1) {
-  # check for factors
-  b = 1
-  for(i in 2:(num-1)) {
-    if ((num %% i) == 0) {
-      b = 0
-      break
-    }
-  }
-} 
-
-if(num == 2)    b = 1
-if(b == 1) {
-  print(paste(num,"is a prime number"))
-} else {
-  print(paste(num,"is not a prime number"))
+#3. Write a function that calculates whether a number is a prime number (hint: what does 2 %% 3 give you?) 
+p_func <- function(num){
+  if(sum(num/1:num==num%/%1:num)==2)
+    print(paste(num, "job: Number"))
 }
+
+#checking the function
+p_func(7)
+
+#credit: http://stackoverflow.com/questions/19767408/prime-number-function-in-r
 
 #4 Write a loop that prints out the numbers from 1 to 20, printing “Good: NUMBER” if the number is divisible by five and “Job: NUMBER” if then number is prime, and nothing otherwise.
 
 for(num in 1:20){
-b = 0
-if(num > 1) {
-  # check for factors
-  b = 1
-  for(i in 2:(num-1)) {
-    if ((num %% i) == 0) {
-      b = 0
-      break
+  if(num%%5==0) {
+    print(paste(num, "Good: number")) } else {
+      p_func(num)
     }
-    if(num%%5==0){
-      b= 0
-    
-    }
-  }
-} 
-
-if(num == 2)    b = 1
-if(b == 1) {
-  print(paste("Prime: number"))
-} else {
-  print(paste("Good:number"))
-}
 }
 
 
 
 
-for(i in 1:20){
-  if(i%%5==0)
-  print(i)
-}
+
+
