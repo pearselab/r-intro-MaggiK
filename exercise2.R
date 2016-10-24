a079556f124600232a8e953c970b52adb607cf4e
Maggi <maggi@Maggis-MacBook-Pro.local>
2016-10-24 10:43:40 -0600

    exercise 2

---
+++ exercise2.R
@@ -0,0 +1,38 @@
+#Programming for Biologists 10/24
+#control flow exercises
+
+#1. Write a loop that prints out the numbers from 20 to 10
+for (i in 20:10){
+  print(i)
+}
+
+#2. Write a loop that print out only the numbers from 20 to 10 that are even
+for(i in 20:10){
+  if(i%%2 ==0)
+  print(i)
+}
+
+#3. Write a function that calculates whether a number is a prime number (hint: what does 2 %% 3 give you?)
+num = as.integer(4)
+
+b = 0
+if(num > 1) {
+  # check for factors
+  b = 1
+  for(i in 2:(num-1)) {
+    if ((num %% i) == 0) {
+      b = 0
+      break
+    }
+  }
+} 
+
+if(num == 2)    b = 1
+if(b == 1) {
+  print(paste(num,"is a prime number"))
+} else {
+  print(paste(num,"is not a prime number"))
+}
+
+#Write a loop that prints out the numbers from 1 to 20, printing “Good: NUMBER” if the number is divisible by five and “Job: NUMBER” if then number is prime, and nothing otherwise.
+
