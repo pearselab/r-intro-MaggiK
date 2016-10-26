c9917041d8a37cb504de929df298158ffe88172d
Maggi <maggi@Maggis-MacBook-Pro.local>
2016-10-26 11:17:31 -0600

    karen helped me

--- exercise2.R
+++ exercise2.R
@@ -33,27 +33,42 @@ for(num in 1:20){
 }
 
 #5. A biologist is modelling population growth using a Gompertz curve, which is defined as y(t) = a.e−b.e−c.t where y is population size, t is time, a and b are parameters, and e is the exponent al function. Write them a function that calculates population size at any time for any values of its parameters.
-a= 4 #carrying capacity
-b= 3 #displacement rate
-c= 1 #population growth rate
-t= 5
-pop_growth=function(a, b, c, t){
-a*exp((-b)*exp((-c)*t)) 
+pop_growth=function(a, b, c, d){
+    a*exp((-b)*exp((-c)*i))
 }
+pop_growth(280,100,.4,30)#Thank you Karen!!
 
 #plugging in random numbers
 y=pop_growth(a,b,c,t)
 y
 #6. The biologist likes your function so much they want you to write another function that plots the progress of the population over a given length of time. Write it for them.
-pop_plot=function(t, y){
-plot(t, y, xlab= "Time", ylab="Population Growth")
+pop_growth=function(a, b, c, d){
+  t<-0
+  vector<-0
+  tme= c(1:d)
+  for (i in tme){
+    t=c(t, i)
+    pop <- a*exp((-b)*exp((-c)*i))
+    vector= c(vector, pop)
+  }
+  plot(t, vector, xlab= "Time", ylab="Population Growth")
 }
-pop_plot(5, y)
+pop_growth(280,100,.4,30)#Thank you Karen!!
 
 #7. The biologist has fallen in love with your plotting function, but want to colour y values above a as blue, and y values above b as red. Change your function to allow that.
-pop_plot=function(t,y){
-plot(t, y, xlab= "Time", ylab="Population Growth", col=ifelse(a<y, "blue", ifelse(y>b, "red", "black")))
+pop_growth=function(a, b, c, d){
+  t<-0
+  vector<-0
+  tme= c(1:d)
+  for (i in tme){
+    t=c(t, i)
+    pop <- a*exp((-b)*exp((-c)*i))
+    vector= c(vector, pop)
+  }
+  plot(t, vector, xlab= "Time", ylab="Population Growth", col=ifelse(a<vector, "blue", ifelse(vector>b, "red", "black")))
 }
+pop_growth(280,100,.4,30)#Thank you Karen!!
+
 #8. You are beginning to suspect the biologist is taking advantage of you. Modify your function to plot in purple any y value that’s above a and b. Hint: try putting 3==3 & 2==2 and 3==4 | 2==2 into an if statement and see what you get. Using this construction may make this simpler.
 pop_plot=function(t,y){
   plot(t, y, xlab= "Time", ylab="Population Growth", col=ifelse(y>a & y>b , "purple", "black"))
@@ -61,8 +76,11 @@ pop_plot=function(t,y){
 pop_plot(5, y)
 
 #9. Write a function that draws boxes of a specified width and height that look like this (height 3, width 5):
+#x values of the points
 x<-c(1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,1.0,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.0)
+#y values of the points
 y<-c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,2.0)
+#creating a function to plot the x and y values to display a box
 bx_func<-function(x,y){
 plot(x,y, type="n", axes=F, ann=F, xlim=c(0,10), ylim=c(0,10), pch=8)
 points(x,y, pch=8)
@@ -71,29 +89,42 @@ points(x,y, pch=8)
 #10. Modify your box function to put text centred inside the box, like this:
 x<-c(1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,1.0,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.0)
 y<-c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,2.0)
-bx_func<-function(x,y){
+tx<- "smile"
+#changing the function to include text in the middle
+bx_func<-function(x,y,tx){
   plot(x,y, type="n", axes=F, ann=F, xlim=c(0,10), ylim=c(0,10), pch=8)
   points(x,y, pch=8)
   text(1.5,2, labels="some text", pos=4)
 }
-bx_func(x,y)
+bx_func(x,y,tx)
 
 #11. Modify your box function to build boxes of arbitrary text, taking dimensions specified in terms of dimensions, not the text. For example, box("wdp", 3, 9, "hey") might produce:
 x<-c(1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,1.0,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.0)
 y<-c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,2.0)
-tx= "some text"
+tx<- "smile"
+
 bx_func<-function(x,y,tx){
-  plot(x,y, type="n", axes=F, ann=F, xlim=c(0,10), ylim=c(0,10), pch=8)
-  points(x,y, pch=8)
-  text(1.5,2, labels=tx, pos=4)
+  plot(x,y, type="n", axes=F, ann=F, xlim=c(0,10), ylim=c(0,10))
+  text(x,y, labels="ski", cex=.5)
+  text(2,2, labels=tx, pos=4)
 }
 bx_func(x,y,tx)
 
+cat("hey\n\n")
+
+
+##random stuff
 plot(x,y, type="n", axes=F, ann=F, xlim=c(0,10), ylim=c(0,10))
-points(x,y, pch=8)
+text(x,y,label="wdp",col='blue', cex=.5)
+points(x,y, pch=)
 par(fin=.3,.5)
 
 plot(-1:1, -1:1, type = "n", xlab = "Re", ylab = "Im")
 K <- 16; text(exp(1i * 2 * pi * (1:K) / K), col = 2)
 
+dat <- data.frame(x=rnorm(100), y1=rnorm(100)-1, y2=rnorm(100), y3=rnorm(100)+1)
+plot(y1 ~ x, data=dat, type='n', ylim=c(-4, 4))     
+text(dat$x,dat$y1,label=0,col='blue')
+text(dat$x,dat$y2,label=1,col='green')
+text(dat$x,dat$y3,label=2,,col='red')
 
